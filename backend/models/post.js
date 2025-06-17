3a45d649b899ce5e1817720a4a0eaf6e5c36557d
Alekhya07-Nam <alekhyanam@gmail.com>
2025-06-17 18:08:08 +0530

    Your commit message

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,17 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const postSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   role: { type: String, required: true },
-//   profilePicture: { type: String },
-//   title: { type: String, required: true },
-//   description: { type: String, required: true },
-//   likes: { type: Number, default: 0 },
-//   createdAt: { type: Date, default: Date.now },
-// });
-
-// module.exports = mongoose.model("Post", postSchema);
-
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
