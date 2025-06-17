c25822363ed2fc02767a83af5fb71bcd5931fbad
rohithakanta744 <rohithakanta07@gmail.com>
2025-06-17 22:50:14 +0530

    alekhya2 merged

--- backend/routes/nodeMailerRoute.js
+++ backend/routes/nodeMailerRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const Route = express.Router();
-const EntireController = require("../Controllers/NodeMailer");
+const EntireController = require("../controllers/nodeMailerController");
 Route.post("/mailSender", EntireController.MailSender);
 module.exports = Route;
