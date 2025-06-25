4cabac4ed08ada999a71d16374b9e993ebcc9e18
Charanraju18 <charanraju925@gmail.com>
2025-06-25 15:56:35 +0530

    updates

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,30 +1,12 @@
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   mongoose
-//     .connect(process.env.MONGO_URI)
-//     .then((conn) => {
-//       console.log(`Connected to MongoDB: ${conn.connection.name}`);
-//     })
-//     .catch((err) => console.error("MongoDB connection error:", err));
-// };
-
-// module.exports = connectDB;
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("✅ MongoDB Connected via Mongoose");
-  } catch (err) {
-    console.error("MongoDB connection error:", err);
-    process.exit(1);
-  }
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then((conn) => {
+      console.log(`Connected to MongoDB: ${conn.connection.name}`);
+    })
+    .catch((err) => console.error("MongoDB connection error:", err));
 };
 
 module.exports = connectDB;
-
-
