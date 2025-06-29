e18422bc78a87fe075899a9a72b068b0484237cd
Charanraju18 <charanraju925@gmail.com>
2025-06-29 17:03:39 +0530

    deploy issues -3

--- backend/app.js
+++ backend/app.js
@@ -42,7 +42,7 @@ app.use((err, req, res, next) => {
 
 app.use(express.static(path.join(__dirname, "eduall", "build")));
 
-app.use("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "eduall", "build", "index.html"));
 });
 
